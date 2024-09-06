2f43e582f8268bf0b0221a5c132ed2d278d78629
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-09-06 10:16:59 -0300

    Enviando o Primeiro Email

--- app/Http/Controllers/Mails/AuthMailController.php
+++ app/Http/Controllers/Mails/AuthMailController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Mails;
 
 use App\Http\Controllers\Controller;
+use App\Mail\RegisterMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 
 class AuthMailController extends Controller
 {
     //
     public function sendRegisterMail(){
+        $registerEmail = new RegisterMail();
 
+        Mail::to('galvaoalveselias@gmail.com')->send($registerEmail);
     }
 }
---
+++ resources/views/Mail/registerMail.blade.php
@@ -0,0 +1,1 @@
+<h1>Seja bem vido filho</h1>
