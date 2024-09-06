f26b57964371a128002585b5a23d6308aaa75766
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-09-06 10:03:56 -0300

    configurando o servidor de emails

---
+++ app/Http/Controllers/Mails/AuthMailController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Mails;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthMailController extends Controller
+{
+    //
+    public function sendRegisterMail(){
+
+    }
+}
